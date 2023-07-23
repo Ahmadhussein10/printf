12cca37c5e000dabb2af3007dc0b18616907367e
ahmadhussein10 <itsahmdhussein19@gmail.com>
2023-07-23 04:35:18 -0700

    update

--- get_precision.c
+++ get_precision.c
@@ -11,7 +11,7 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = 1;
+	int precision = - 1;
 
 	if (format[curr_i] != '.')
 		return (precision);
@@ -35,7 +35,7 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = curr_i + 1;
 
 	return (precision);
 }
